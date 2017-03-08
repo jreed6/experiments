b3401b1ebf51b2c222eb8af9c3ed3a208ea0b772
Reed <jbreed@gs612-reed.ndc.nasa.gov>
2017-03-08 16:50:21 -0500

    fixing _makeindex, syncing with remote

---
+++ drawing/script.js
@@ -0,0 +1,11 @@
+
+function setup(){
+  createCanvas(400,400);
+  print("setup function!");
+}
+
+function draw(){
+  background(255,0,255);
+  ellipse(200,200,50,50);
+  console.log(frameCount);
+}
