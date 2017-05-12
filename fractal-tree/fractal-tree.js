a327fdab30e4d784c1af8b62103630cb0132d8e6
Jacob Reed <jreed6@users.noreply.github.com>
2017-05-12 15:45:59 -0400

    Update fractal-tree.js

--- fractal-tree/fractal-tree.js
+++ fractal-tree/fractal-tree.js
@@ -31,15 +31,13 @@ function mousePressed(){
       leaves.push(leaf);
     }
   }
-  if(count == 2){
-      scareML();
-  }
+ 
 }
 
 function scareML(){
     textSize(42);   
-    fill(100, 0, 0);
-    text("ARGHH MARYLYNN YOU BROKE IT!!! WHAT DID YOU DOO??????", 10, 60);
+    fill(155, 0, 0);
+    text("ARGHH MARYLYNN YOU BROKE\n IT!!! WHAT DID YOU\n DOO??????", 10, 60);
 }
 
 function draw() {
@@ -55,7 +53,9 @@ function draw() {
       ellipse(leaves[i].x, leaves[i].y, 8, 8);
       leaves[i].y += random(0, 1);
     }
-     scareML();
+     if(count == 2){
+      scareML();
+  }
 }
 
 function branch(len) {
